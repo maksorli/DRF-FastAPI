9106f83ff2e85bd77f180c80e174eba88da66cd1
MaksimEvdokimov <maksorli@gmail.com>
2024-09-27 12:35:56 +0400

    change README.MD

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# DRF-FastAPI
+# Simple DRF
+![Python](https://img.shields.io/badge/Python-3.10-blue?style=flat&logo=python)
+![Django](https://img.shields.io/badge/Django-5.1.1-green)
+![DRF](https://img.shields.io/badge/DRF-3.15.2-blue)
+![PostgreSQL](https://img.shields.io/badge/PostgreSQL-14-blue?style=flat&logo=postgresql)
+![Docker](https://img.shields.io/badge/Docker-20.10-blue?style=flat&logo=docker)
+![black](https://img.shields.io/badge/black-24.8.0-black?style=flat&logo=python)
+
+ 
+## Описание проекта
+
+Этот проект является тестовым заданием. Он предоставляет простой API для управления списком задач. Он построен с использованием Django и Django REST Framework, что позволяет  выполнять операции создания, чтения, обновления и удаления (CRUD) для задач.
+В качестве БД используется PostgreSQL
+
+## Функциональность
+
+API предоставляет следующие возможности:
+
+- **Получение списка задач** (GET)
+- **Создание новой задачи** (POST)
+- **Обновление существующей задачи** (PUT/PATCH)
+- **Удаление задачи** (DELETE)
+
+## Модель Task
+
+Модель `Task` имеет следующие поля:
+
+- `title`: Строка, длиной до 100 символов, обязательное поле.
+- `description`: Текстовое поле, необязательное.
+- `completed`: Булево значение, по умолчанию `False`.
+
+## Установка и запуск
+
+### Запуск
+
+
+1. Клонируйте терепозиторий
+
+    ```bash
+    git clone https://github.com/yourusername/task-management-api.git
+    cd task-management-api
+2. Проверьте версию Docker и Docker Compose, либо установите:
+    ```bash
+    docker --version
+    docker-compose --version
+3. Запустите проект с помощью Docker Compose:
+   ```bash
+   docker-compose up --build
--- tasks/tests.py
+++ tasks/tests.py
@@ -3,38 +3,41 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from .models import Task
 
+
 class TaskAPITestCase(TransactionTestCase):
     def setUp(self):
         self.client = APIClient()
-        self.task_data = {'title': 'Test Task', 'description': 'Test Description'}
-        self.response = self.client.post('/api/tasks/', self.task_data, format='json')
+        self.task_data = {"title": "Test Task", "description": "Test Description"}
+        self.response = self.client.post("/api/tasks/", self.task_data, format="json")
 
     def test_create_task(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Task.objects.count(), 1)
-        self.assertEqual(Task.objects.get().title, 'Test Task')
+        self.assertEqual(Task.objects.get().title, "Test Task")
 
     def test_get_task_list(self):
-        response = self.client.get('/api/tasks/')
+        response = self.client.get("/api/tasks/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
     def test_get_task_detail(self):
         task = Task.objects.get()
-        response = self.client.get(f'/api/tasks/{task.id}/')
+        response = self.client.get(f"/api/tasks/{task.id}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'Test Task')
+        self.assertEqual(response.data["title"], "Test Task")
 
     def test_update_task(self):
         task = Task.objects.get()
-        updated_data = {'title': 'Updated Task', 'completed': True}
-        response = self.client.patch(f'/api/tasks/{task.id}/', updated_data, format='json')
+        updated_data = {"title": "Updated Task", "completed": True}
+        response = self.client.patch(
+            f"/api/tasks/{task.id}/", updated_data, format="json"
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Task.objects.get().title, 'Updated Task')
+        self.assertEqual(Task.objects.get().title, "Updated Task")
         self.assertTrue(Task.objects.get().completed)
 
     def test_delete_task(self):
         task = Task.objects.get()
-        response = self.client.delete(f'/api/tasks/{task.id}/')
+        response = self.client.delete(f"/api/tasks/{task.id}/")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Task.objects.count(), 0)
